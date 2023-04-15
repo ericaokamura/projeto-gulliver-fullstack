97bdab0d985ee1c5cb49458e8379a187f82d1afd
ericaokamura <erica.okamura@yahoo.com.br>
2023-04-15 05:15:36 -0300

    adicionando bean validation

--- src/main/java/com/gulliver/projeto/usuario/projetogulliverusuario/model/Usuario.java
+++ src/main/java/com/gulliver/projeto/usuario/projetogulliverusuario/model/Usuario.java
@@ -1,11 +1,14 @@
 package com.gulliver.projeto.usuario.projetogulliverusuario.model;
 
+import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.util.Date;
 
 @Entity
 @Data
@@ -14,9 +17,15 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotEmpty
     private String nomeCompleto;
+    @NotEmpty
     private String login;
+    @NotEmpty
+    @Size(min = 8, max = 200, message = "Senha deve ter no mínimo 8 caracteres")
     private String senha;
+    @NotEmpty
+    @Email(message = "Email deve ser válido")
     private String email;
 
 }

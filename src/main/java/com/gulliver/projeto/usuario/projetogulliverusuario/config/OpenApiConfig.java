8a439895a32b6d09fa2c79fd5f703378110d68d9
ericaokamura <erica.okamura@yahoo.com.br>
2023-04-15 06:40:32 -0300

    adicionando configs do openapi

---
+++ src/main/java/com/gulliver/projeto/usuario/projetogulliverusuario/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.gulliver.projeto.usuario.projetogulliverusuario.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("API Projeto Gulliver")
+                        .description("Projeto Gulliver - API - Usuários")
+                        .termsOfService("terms")
+                        .license(new License().name("GNU"))
+                        .version("1.0")
+                );
+    }
+}
